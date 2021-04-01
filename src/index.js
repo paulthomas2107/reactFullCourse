dc0011bfb5723469bfa9ef9c8f54b3571ea36bf8
Paul Thomas <paulthomas2107@gmail.com>
2021-04-01 16:27:04 +0100

    more

--- src/index.js
+++ src/index.js
@@ -1,24 +1,42 @@
 import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
+// setup
+const books = [
+  {
+    id: 1000,
+    img: "https://m.media-amazon.com/images/I/51XKn0Ua3gL.jpg",
+    title: "And Away...",
+    author: "Bob Mortimer",
+  },
+  {
+    id: 2000,
+    img: "https://m.media-amazon.com/images/I/51XKn0Ua3gL.jpg",
+    title: "And Away V2.0",
+    author: "Bob Mortimer and Anon",
+  },
+];
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
+      {books.map((book) => {
+        return (
+          <Book key={book.id} book={book}></Book>
+        );
+      })}
     </section>
   );
 }
 
-const Book = () => {
-  const title = "And Away...";
-  const author = "Bob Mortimer";
-  const image = "https://m.media-amazon.com/images/I/51XKn0Ua3gL.jpg";
+const Book = (props) => {
+  const { id, img, author, title } = props.book;
   return (
     <article className="book">
-      <img src={image} alt="" />
-      <h1>{title}/</h1>
-      <h4>{author.toUpperCase()}</h4>
+      <img src={img} alt="" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+      <h4>{id}</h4>
     </article>
   );
 };
